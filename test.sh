eac0793c0b61a4c95a90a6b2fd7a1b9170b880b7
Yuma Hiramatsu <ayzgnm5k8y@gmail.com>
2018-10-29 02:08:56 +0900

    remove unnecessary file

--- test.sh
+++
@@ -1,8 +0,0 @@
-python3 main.py --input test_images/straight_lines1.jpg --output output_images/straight_lines1.jpg
-python3 main.py --input test_images/straight_lines2.jpg --output output_images/straight_lines2.jpg
-python3 main.py --input test_images/test1.jpg --output output_images/test1.jpg
-python3 main.py --input test_images/test2.jpg --output output_images/test2.jpg
-python3 main.py --input test_images/test3.jpg --output output_images/test3.jpg
-python3 main.py --input test_images/test4.jpg --output output_images/test4.jpg
-python3 main.py --input test_images/test5.jpg --output output_images/test5.jpg
-python3 main.py --input test_images/test6.jpg --output output_images/test6.jpg
